d8272968d64224fbebbfaca43627bdcc0497fa3b
gauravchaturvedi <gkchat10@gmail.com>
2018-03-10 22:31:05 -0500

    Added feature to check sum of category and subcategory weights before calculation

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ var categoryWeights = {
   'economic': 25
 };
 
+var subCategoryGroups = {
+  'environment': ['nOfUnicorns', 'innovRanking', 'vcDealAmt', 'noOfFinTechStartups', 'wifiPercent', 'intAccess'],
+  'social': ['techJobGrowth', 'livableCities'],
+  'political': ['taxRate', 'taxIncentives'],
+  'economic': ['finTechJobCount', 'gfci']
+};
+
 var sCW = {
   'vcDealAmt': 20,
   'innovRanking': 15,
@@ -36,27 +43,14 @@ var mainData = {};
 
 const windowHeight = $( window ).height();
 const headerHeight = $('#headerBar').height()
-$('#tableContainer').height( windowHeight - headerHeight - 25);
+$('#tableContainer').height( windowHeight - headerHeight - 35);
+$('#weightsBar').height( windowHeight - headerHeight - 35);
+// $('#myModal').modal('show');
+// $('#myModal').modal({ show: false});
 
 function categoryWeightUpdated(id, elem) {
   const newValue = elem.value;
-  // const comparisonObject = Object.assign({}, categoryWeights);
-  // comparisonObject[id] = newValue;
-  // var sum = 0;
-  // for (var key in comparisonObject) {
-  //   if (comparisonObject.hasOwnProperty(key)) {
-  //     sum += parseFloat(comparisonObject[key]);
-  //   }
-  // }
-
   categoryWeights[id] = (parseFloat(newValue) || 0);
-
-  // if (sum == 100) {
-  //   // categoryWeights[id] = newValue;
-  // } else {
-  //   // Put up error message
-  //   // $('#' + id)[0].value  = categoryWeights[id];
-  // }
 }
 
 function subCategoryWeightUpdated(id, elem) {
@@ -109,21 +103,59 @@ function setCategoryToZero(category) {
 }
 
 function calculateRankings() {
-  for (var key in mainData) {
-    const nObj = mainData[key];
-    mainData[key].Score = categoryWeights.environment*(parseFloat(nObj['# of Unicorn'])*sCW.nOfUnicorns/100 + parseFloat(nObj['Innovation Ranking'])*sCW.innovRanking/100);
-    mainData[key].Score += categoryWeights.environment*(parseFloat(nObj['VC deal $ amount'])*sCW.vcDealAmt/100 + parseFloat(nObj['Number of FinTech Startups'])*sCW.noOfFinTechStartups/100);
-    mainData[key].Score += categoryWeights.environment*(parseFloat(nObj['Internet Access'])*sCW.intAccess/100 + parseFloat(nObj['Wifi (% of time spent connected to wifi networks)'])*sCW.wifiPercent/100);
-    mainData[key].Score += categoryWeights.social*(parseFloat(nObj["Tech Jobs Growth Rate % (06'-16')"])*sCW.techJobGrowth/100 + parseFloat(nObj['Most Liveable Cities'])*sCW.livableCities/100);
-    mainData[key].Score += categoryWeights.political*(parseFloat(nObj["Effective State Corporate Tax Rate 2017"])*sCW.taxRate/100 + parseFloat(nObj['Tax Incentives'])*sCW.taxIncentives/100);
-    mainData[key].Score += categoryWeights.economic*(parseFloat(nObj["Fintech Job Count"])*sCW.finTechJobCount/100 + parseFloat(nObj['GFCI'])*sCW.gfci/100);
-    mainData[key].Score = (mainData[key].Score/100).toFixed(2);
+  // Check sum of weights
+  var categorySum = 0;
+  var categorySumError = false;
+  var subCategorySumError = [];
+  // Check for sum of category weights and the subcategories of each category
+  for (var key in categoryWeights) {
+    if (categoryWeights.hasOwnProperty(key)) {
+        categorySum += categoryWeights[key];
+        if (categoryWeights[key] !== 0) {
+          var subCategorySum = 0;
+          // Find sum of all the subCategories for this category
+          subCategoryGroups[key].forEach(function(subCategory,i ) {
+            subCategorySum += sCW[subCategory];
+          });
+          if (subCategorySum > 100 || subCategorySum < 100) {
+            subCategorySumError.push(key)
+          }
+        }
+    }
   }
 
-  mainData.sort(function(a, b){
-    return b.Score-a.Score;
-  });
-  generateTable();
+  // Show flag for sum error in subCategories
+  if (categorySum > 100 || categorySum < 100) {
+    categorySumError = true;
+    // $('#categoryErrorMessage').show();
+    $('#categoryErrorModal').modal('show');
+  }
+
+  // Show flag for sum error in subCategories
+  if (subCategorySumError.length !== 0) {
+    $('#subCategoryErrorModal').modal('show');
+    $('#subCategoryErrorPlaceholder').text(' for the following categories: ' + subCategorySumError.join(', '));
+  }
+
+  // Only run the calculations if the sum of all category and subcategory weights are correct
+  if (!categorySumError && subCategorySumError.length < 1) {
+
+    for (var key in mainData) {
+      const nObj = mainData[key];
+      mainData[key].Score = categoryWeights.environment*(parseFloat(nObj['# of Unicorn'])*sCW.nOfUnicorns/100 + parseFloat(nObj['Innovation Ranking'])*sCW.innovRanking/100);
+      mainData[key].Score += categoryWeights.environment*(parseFloat(nObj['VC deal $ amount'])*sCW.vcDealAmt/100 + parseFloat(nObj['Number of FinTech Startups'])*sCW.noOfFinTechStartups/100);
+      mainData[key].Score += categoryWeights.environment*(parseFloat(nObj['Internet Access'])*sCW.intAccess/100 + parseFloat(nObj['Wifi (% of time spent connected to wifi networks)'])*sCW.wifiPercent/100);
+      mainData[key].Score += categoryWeights.social*(parseFloat(nObj["Tech Jobs Growth Rate % (06'-16')"])*sCW.techJobGrowth/100 + parseFloat(nObj['Most Liveable Cities'])*sCW.livableCities/100);
+      mainData[key].Score += categoryWeights.political*(parseFloat(nObj["Effective State Corporate Tax Rate 2017"])*sCW.taxRate/100 + parseFloat(nObj['Tax Incentives'])*sCW.taxIncentives/100);
+      mainData[key].Score += categoryWeights.economic*(parseFloat(nObj["Fintech Job Count"])*sCW.finTechJobCount/100 + parseFloat(nObj['GFCI'])*sCW.gfci/100);
+      mainData[key].Score = (mainData[key].Score/100).toFixed(2);
+    }
+    mainData.sort(function(a, b){
+      return b.Score-a.Score;
+    });
+    generateTable();
+  }
+
 }
 
 function generateTable() {
